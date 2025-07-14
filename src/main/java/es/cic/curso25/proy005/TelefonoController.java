038c70781cd2201eff1d4a7feede9ef72b11f625
HeDichoQueNo <no@nomail.no>
2025-07-14 11:26:32 +0200

    Primer api de ejemlo

---
+++ src/main/java/es/cic/curso25/proy005/TelefonoController.java
@@ -0,0 +1,16 @@
+package es.cic.curso25.proy005;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/telefono")
+public class TelefonoController {
+
+    @GetMapping
+    public String mensaje(){
+        return "Holi";
+    }
+
+}
