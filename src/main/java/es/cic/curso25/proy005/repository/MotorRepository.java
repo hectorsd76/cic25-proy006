96e6c205faca72ff703823777a1509cf58b17c74
nomail <no@nomail.no>
2025-07-17 14:10:17 +0200

    Nuestro servicio está hecho de verdad, aunque aun no está el controlador completo

--- src/main/java/es/cic/curso25/proy005/repository/MotorRepository.java
+++ src/main/java/es/cic/curso25/proy005/repository/MotorRepository.java
@@ -1,55 +1,10 @@
 package es.cic.curso25.proy005.repository;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import es.cic.curso25.proy005.model.Motor;
 
-@Repository
-public class MotorRepository {
+public interface MotorRepository extends JpaRepository<Motor, Long> {
 
-    private Map<Long, Motor> motores = new HashMap<>();
-
-    public long create(Motor motor) {
-        long mayor = getSiguienteId();
-        motor.setId(mayor);
-        motores.put(motor.getId(), motor);
-        return motor.getId();
-    }
-
-    private long getSiguienteIdConStreams() {
-        long mayor = 
-            motores
-                .keySet()
-                .stream()
-                .max(
-                    (primero, segundo) -> (int) (segundo.longValue() - primero.longValue())
-                ).get();
-        return mayor + 1;        
-    }
-
-
-
-    private long getSiguienteId() {
-        long mayor = 0;
-        
-        Set<Long> ids = motores.keySet();
-        
-        Iterator<Long> iteratorId =  ids.iterator();
-
-
-        while (iteratorId.hasNext()) {
-            Long siguiente = iteratorId.next();
-            if (siguiente.longValue() > mayor) {
-                mayor = siguiente;
-            }
-        }
-        mayor++;
-        return mayor;
-    }
 }
