3c2e4ca194e685c752a15a000bde5324d531c09b
hsolana <hsolana@formacion.cic.es>
2025-07-18 08:17:40 +0200

    Nuestro servicio está hecho de verdad, aunque aun no está el controlador completo

--- src/main/java/es/cic/curso25/proy005/Proy005Application.java
+++ src/main/java/es/cic/curso25/proy005/Proy005Application.java
@@ -1,12 +1,18 @@
 package es.cic.curso25.proy005;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Proy005Application {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Proy005Application.class);
+
 	public static void main(String[] args) {
+        LOGGER.error("Traza de error");
+
 		SpringApplication.run(Proy005Application.class, args);
 	}
 
--- src/main/java/es/cic/curso25/proy005/service/MotorService.java
+++ src/main/java/es/cic/curso25/proy005/service/MotorService.java
@@ -1,5 +1,8 @@
 package es.cic.curso25.proy005.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +17,25 @@ public class MotorService {
 
     public long create(Motor motor) {
         // Los días festivos y los fines de semana en esta empresa no se crean motores
+        motorRepository.save(motor);
 
-        return motorRepository.create(motor);
+        return motor.getId();
+    }
+
+    public Motor get(long id) {
+        Optional<Motor> resultado = motorRepository.findById(id);
+        return resultado.orElse(null);
+    }
+
+    public List<Motor> get() {
+        return motorRepository.findAll();
+    }
+
+    public void update(Motor motor) {
+        motorRepository.save(motor);
+    }
+
+    public void delete(long id) {
+        motorRepository.deleteById(id);
     }
 }
